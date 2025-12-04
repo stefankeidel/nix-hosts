96b7ebfc4a1b257a3a1a2ecfc0049cd003a7ae83
Stefan Keidel <1188614+stefankeidel@users.noreply.github.com>
2025-12-04 14:11:15 +0100

    add work laptop pkgs

--- hosts/lichtblick/users/stefan.keidel@lichtblick.de/home-configuration.nix
+++ hosts/lichtblick/users/stefan.keidel@lichtblick.de/home-configuration.nix
@@ -5,4 +5,22 @@
     inputs.self.homeModules.home-shared
     inputs.self.homeModules.home-darwin
   ];
+
+  home = {
+    packages = with pkgs; [
+      duckdb
+      k9s
+      kubectl
+      kubectx
+      kubernetes-helm
+      kubelogin
+      colima
+      (writeShellScriptBin "do_bak" ''
+        #!/usr/bin/env zsh
+        set -e
+        restic --password-file ~/.config/restic-pw --repo rclone:sb:lichtblick-bak backup ~/code ~/Documents ~/Desktop ~/Nextcloud --skip-if-unchanged
+        restic --password-file ~/.config/restic-pw --repo rclone:sb:lichtblick-bak forget --keep-daily 7 --keep-weekly 4 --keep-monthly 12 --prune
+      '')
+    ];
+  };
 }
