00f8175820daf125fbb21c8731ccabae7ed1f58c
Stefan Keidel <1188614+stefankeidel@users.noreply.github.com>
2025-12-06 08:11:40 +0100

    rm podman

--- hosts/nextcloud/configuration.nix
+++ hosts/nextcloud/configuration.nix
@@ -24,7 +24,7 @@
 
   users.users.stefan = {
     isNormalUser = true;
-    extraGroups = ["wheel" "podman"];
+    extraGroups = ["wheel"];
 
     openssh.authorizedKeys.keys = [''ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAwU52M/vXuUkthu481OGKYMzFGwc9GfjvVwDLt7yQGeDXUZHx5tpL2NEKSS3imnTfOJp25wFTOAJdF63eznIOUEc+5dCZe8xeZ7IMASGlNQJy51sNUlx986BIjYxLbCl0tykkySs82ZNaog9BapjxiHm2tXb1LFR2CsGOg9mLqRVNxQkOj8KkX5+r/NhVxQRFFW8OJn7rgqsyJtA7vKRwEP+nUsokO3cr/+sWeW7APgrnnkh9iYr/ZG6ibZH/m1+t4yW1kcENVy2X8Gyrs0GWMYQCLrBB+zJYBdwxBdeWSt76QlZnOpdwWcaZEC5PUVzTiKtyUok2NjBoqdpnLezrDw=='' ];
   };
