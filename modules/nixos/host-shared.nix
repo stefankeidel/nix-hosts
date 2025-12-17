ca8a22e946667b28a4ab29fd5231e7cf19e2d463
Stefan Keidel <1188614+stefankeidel@users.noreply.github.com>
2025-12-17 17:55:52 +0100

    +nh

--- modules/nixos/host-shared.nix
+++ modules/nixos/host-shared.nix
@@ -2,9 +2,10 @@
   # you can check if host is darwin by using pkgs.stdenv.isDarwin
   environment.systemPackages = [
     pkgs.btop
-    pkgs.htop
     pkgs.git
+    pkgs.htop
     pkgs.inetutils
+    pkgs.nh
     pkgs.ripgrep
   ]; # ++ (pkgs.lib.optionals pkgs.stdenv.isDarwin [ pkgs.xbar ]);
 }
