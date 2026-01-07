725a0bc2022afbde8667e6f3e6abee5c260c5f68
Stefan Keidel <1188614+stefankeidel@users.noreply.github.com>
2026-01-07 19:12:58 +0100

    some more guest fun

--- hosts/nixguest-mini/configuration.nix
+++ hosts/nixguest-mini/configuration.nix
@@ -60,8 +60,9 @@
     variant = "";
   };
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
+  security.sudo.wheelNeedsPassword = false;
+
+  services.tailscale.enable = true;
 
   # Enable sound with pipewire.
   services.pulseaudio.enable = false;
@@ -87,6 +88,8 @@
     isNormalUser = true;
     description = "Stefan";
     extraGroups = [ "networkmanager" "wheel" ];
+    openssh.authorizedKeys.keys = [''ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAwU52M/vXuUkthu481OGKYMzFGwc9GfjvVwDLt7yQGeDXUZHx5tpL2NEKSS3imnTfOJp25wFTOAJdF63eznIOUEc+5dCZe8xeZ7IMASGlNQJy51sNUlx986BIjYxLbCl0tykkySs82ZNaog9BapjxiHm2tXb1LFR2CsGOg9mLqRVNxQkOj8KkX5+r/NhVxQRFFW8OJn7rgqsyJtA7vKRwEP+nUsokO3cr/+sWeW7APgrnnkh9iYr/ZG6ibZH/m1+t4yW1kcENVy2X8Gyrs0GWMYQCLrBB+zJYBdwxBdeWSt76QlZnOpdwWcaZEC5PUVzTiKtyUok2NjBoqdpnLezrDw=='' ];
+
     packages = with pkgs; [
     #  thunderbird
        vim
