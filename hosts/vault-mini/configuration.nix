efefb44b4f52eee99d47a5181e26e4dee74c4dfa
Stefan Keidel <1188614+stefankeidel@users.noreply.github.com>
2026-01-10 23:30:04 +0100

    less often

--- hosts/vault-mini/configuration.nix
+++ hosts/vault-mini/configuration.nix
@@ -95,6 +95,8 @@
       name = "origin";
       url = "https://github.com/stefankeidel/nix-hosts.git";
       branches.main.name = "main";
+      # We want to fetch this remote every 300 seconds
+      poller.period = 300;
     }];
   };
 
