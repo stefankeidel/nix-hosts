f8ba520b0795aa7e439730888c42312fa2bc0eff
Stefan Keidel <1188614+stefankeidel@users.noreply.github.com>
2026-01-11 12:52:02 +0100

    move nixie bak to online only

--- hosts/nixie/backup.nix
+++ hosts/nixie/backup.nix
@@ -24,11 +24,18 @@
 
       /run/current-system/sw/bin/pg_dump -U postgres -d accounting -f /var/lib/nextcloud/accounting.sql
 
-      /run/current-system/sw/bin/restic --password-file /run/agenix/restic --repo /var/bak/restic backup /var/lib/nextcloud /var/lib/actualbudget --skip-if-unchanged
+      /run/current-system/sw/bin/restic \
+        --password-file /run/agenix/restic \
+        --repo rclone:sb:nixie-bak backup \
+        --tag nixie-2026-01 \
+        /var/lib/nextcloud
 
-      /run/current-system/sw/bin/restic --password-file /run/agenix/restic --repo /var/bak/restic forget --keep-daily 7 --keep-weekly 4 --keep-monthly 1 --prune
-
-      /run/current-system/sw/bin/rclone -v sync /var/bak/restic/ sb:nixie-bak/
+      /run/current-system/sw/bin/restic \
+        --password-file /run/agenix/restic \
+        --repo rclone:sb:nixie-bak forget \
+        --tag nixie-2026-01 \
+        --keep-daily 7 --keep-weekly 1 --keep-monthly 1 \
+        --prune
 
       rm -f /var/lib/nextcloud/db_$d.sql
     '';
