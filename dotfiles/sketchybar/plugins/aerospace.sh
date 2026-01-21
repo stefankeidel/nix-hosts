0772d3f299f706d5df8a2b46b839ea82445b37b1
Stefan Keidel <1188614+stefankeidel@users.noreply.github.com>
2026-01-21 19:45:59 +0100

    Reapply "wip"
    This reverts commit ac7fe7ee44d29c1c1bf9526b17451bbc84fdb280.

--- dotfiles/sketchybar/plugins/aerospace.sh
+++ dotfiles/sketchybar/plugins/aerospace.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
+exec >>/tmp/sketchybar-aerospace.log 2>&1
+set -x
+
 workspace="${1:-${NAME#aerospace.}}"
 focused="$FOCUSED_WORKSPACE"
 if [ -z "$focused" ]; then
-  focused="$(aerospace list-workspaces --focused | head -n 1)"
+  focused="$("${AEROSPACE_BIN:-/etc/profiles/per-user/$USER/bin/aerospace}" list-workspaces --focused | head -n 1)"
 fi
 
 if [ "$workspace" = "$focused" ]; then
