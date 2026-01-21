aaa88829ba8328c6ffe6d06a2e993a550f2921a1
Stefan Keidel <1188614+stefankeidel@users.noreply.github.com>
2026-01-21 19:48:00 +0100

    hacky but works

--- dotfiles/sketchybar/plugins/aerospace.sh
+++ dotfiles/sketchybar/plugins/aerospace.sh
@@ -6,7 +6,7 @@ set -x
 workspace="${1:-${NAME#aerospace.}}"
 focused="$FOCUSED_WORKSPACE"
 if [ -z "$focused" ]; then
-  focused="$("${AEROSPACE_BIN:-/etc/profiles/per-user/$USER/bin/aerospace}" list-workspaces --focused | head -n 1)"
+  focused="$("${AEROSPACE_BIN}" list-workspaces --focused | head -n 1)"
 fi
 
 if [ "$workspace" = "$focused" ]; then
