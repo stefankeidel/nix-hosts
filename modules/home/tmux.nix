2877cea031b79c8f78a7ca1b8da40e153d3e095d
Stefan Keidel <1188614+stefankeidel@users.noreply.github.com>
2026-02-10 13:27:18 +0100

    some basic tmux config

--- modules/home/tmux.nix
+++ modules/home/tmux.nix
@@ -7,6 +7,7 @@
     shell = "${pkgs.zsh}/bin/zsh";
     
     terminal = "tmux-256color";
+    keyMode = "emacs";
     
     historyLimit = 100000;
     plugins = with pkgs;
@@ -16,6 +17,17 @@
         tmuxPlugins.tmux-fzf
       ];
     extraConfig = ''
+      # Emacs-friendly prefix
+      unbind C-b
+      set -g prefix C-x
+      bind C-x send-prefix
+
+      # Emacs-like window splitting
+      bind 2 split-window -v
+      bind 3 split-window -h
+      bind 0 kill-pane
+      bind o select-pane -t :.+
+      bind 1 kill-pane -a
     '';
   };
 }
