000d22080e076a462a262de4bce6ff85e2f8b51d
Stefan Keidel <1188614+stefankeidel@users.noreply.github.com>
2026-02-11 06:26:44 +0100

    add switch script

---
+++ dotfiles/raycast/scripts/ghostty_switch.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title ghostty switch
+# @raycast.mode silent
+open -a ~/Applications/Home\ Manager\ Apps/Ghostty.app
