4627b9de9b6f195c14583a0bc98b8012400a5739
Stefan Keidel <1188614+stefankeidel@users.noreply.github.com>
2026-02-25 17:47:20 +0100

    codex fix for https://github.com/nixos/nixpkgs/issues/493775

--- modules/darwin/desktop.nix
+++ modules/darwin/desktop.nix
@@ -34,6 +34,11 @@
   # terraform is unfree :-/
   nixpkgs.config.allowUnfree = true;
 
+  # Workaround for dbus-run-session failures on Darwin (see nixpkgs #493775).
+  nixpkgs.overlays = lib.mkAfter [
+    (import ../../overlays/dbus-darwin-session.nix)
+  ];
+
   # nix linux builder
   nix.linux-builder = {
     enable = true;
