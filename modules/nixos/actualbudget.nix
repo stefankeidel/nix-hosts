495f44e98c218021d0fe7d2039301ec263c90b60
Stefan Keidel <1188614+stefankeidel@users.noreply.github.com>
2026-02-27 19:21:28 +0100

    bump actual

--- modules/nixos/actualbudget.nix
+++ modules/nixos/actualbudget.nix
@@ -5,7 +5,7 @@
   virtualisation.oci-containers = {
     # backend = "docker"; # podman craps out in VM
     containers.actual = {
-      image = "actualbudget/actual-server:26.1.0";
+      image = "actualbudget/actual-server:26.2.1";
 
       # bind to tailnet only
       ports = [
