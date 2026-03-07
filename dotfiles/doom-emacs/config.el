8f819f50f0beeffc5c3bab3e477098f54899905e
Stefan Keidel <1188614+stefankeidel@users.noreply.github.com>
2026-03-07 08:27:54 +0100

    config

--- dotfiles/doom-emacs/config.el
+++ dotfiles/doom-emacs/config.el
@@ -231,7 +231,16 @@
               ("<tab>" . 'copilot-accept-completion)
               ("TAB" . 'copilot-accept-completion)
               ("C-TAB" . 'copilot-accept-completion-by-word)
-              ("C-<tab>" . 'copilot-accept-completion-by-word)))
+              ("C-<tab>" . 'copilot-accept-completion-by-word)
+              ("C-n" . 'copilot-next-completion)
+              ("C-p" . 'copilot-previous-completion))
+
+  :config
+  (add-to-list 'copilot-indentation-alist '(prog-mode 2))
+  (add-to-list 'copilot-indentation-alist '(org-mode 2))
+  (add-to-list 'copilot-indentation-alist '(text-mode 2))
+  (add-to-list 'copilot-indentation-alist '(clojure-mode 2))
+  (add-to-list 'copilot-indentation-alist '(emacs-lisp-mode 2)))
 
 ; my legacy org mode clusterfuck of a configuration
 ; should be at the very bottom and refactored at some point
