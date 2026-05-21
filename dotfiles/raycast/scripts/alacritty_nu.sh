d28204ffc63abca26ecabc928b4a2ca8eb7c800e
Stefan Keidel <1188614+stefankeidel@users.noreply.github.com>
2026-05-21 08:04:07 +0200

    -nu

--- dotfiles/raycast/scripts/alacritty_nu.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Required parameters:
-# @raycast.schemaVersion 1
-# @raycast.title alacrity new nu
-# @raycast.mode silent
-open -n -a ~/Applications/Home\ Manager\ Apps/Alacritty.app --args -e /etc/profiles/per-user/${USER}/bin/nu
--- dotfiles/raycast/scripts/alacritty_zsh.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-# Required parameters:
-# @raycast.schemaVersion 1
-# @raycast.title alacrity new zsh
-# @raycast.mode silent
-open -n -a ~/Applications/Home\ Manager\ Apps/Alacritty.app
