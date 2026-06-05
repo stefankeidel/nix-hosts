caa5e99baf7c7706a334b98a90949c3afabc1e35
Stefan Keidel <1188614+stefankeidel@users.noreply.github.com>
2026-06-05 21:45:25 +0200

    some more cleanup

--- dotfiles/sketchybar/plugins/teams.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-status="$(lsappinfo -all info -only StatusLabel "Microsoft Teams" | sed -nr 's/\"StatusLabel\"=\{ \"label\"=\"(.+)\" \}$/\1/p')"
-
-if [ -z "$status" ] || [ "$status" -eq 0 ] 2>/dev/null; then
-  sketchybar --set "$NAME" label="" label.drawing=off
-else
-  sketchybar --set "$NAME" label="T $status" label.drawing=on
-fi
