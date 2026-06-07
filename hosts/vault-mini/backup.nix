e4a2470b67616273db0b3f285aaf6f80352e682d
Stefan Keidel <1188614+stefankeidel@users.noreply.github.com>
2026-06-07 14:19:34 +0200

    bak

--- hosts/vault-mini/backup.nix
+++ hosts/vault-mini/backup.nix
@@ -17,7 +17,11 @@
         --password-file /run/agenix/restic \
         --repo rclone:sb:vault-mini-bak backup \
         --tag vault-mini-2026-01 \
-        /var/lib/actualbudget
+        /var/lib/actualbudget \
+        /mnt/share/Share/Immich/backups \
+        /mnt/share/Share/Immich/library \
+        /mnt/share/Share/Immich/profile \
+        /mnt/share/Share/Immich/upload
 
       /run/current-system/sw/bin/restic \
         --password-file /run/agenix/restic \
@@ -30,5 +34,6 @@
       Type = "oneshot";
       User = "root";
     };
+    unitConfig.RequiresMountsFor = "/mnt/share/Share/Immich";
   };
 }
