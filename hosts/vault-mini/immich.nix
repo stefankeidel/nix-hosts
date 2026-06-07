63afba5ec27dbbad62b8686671d16ce12b81c1e8
Stefan Keidel <1188614+stefankeidel@users.noreply.github.com>
2026-06-07 12:54:40 +0200

    yalla

--- hosts/vault-mini/immich.nix
+++ hosts/vault-mini/immich.nix
@@ -3,6 +3,18 @@
 {
   systemd.tmpfiles.rules = [
     "d /mnt/share/Share/Immich 0700 immich immich -"
+    "d /mnt/share/Share/Immich/backups 0700 immich immich -"
+    "d /mnt/share/Share/Immich/encoded-video 0700 immich immich -"
+    "d /mnt/share/Share/Immich/library 0700 immich immich -"
+    "d /mnt/share/Share/Immich/profile 0700 immich immich -"
+    "d /mnt/share/Share/Immich/thumbs 0700 immich immich -"
+    "d /mnt/share/Share/Immich/upload 0700 immich immich -"
+    "f /mnt/share/Share/Immich/backups/.immich 0600 immich immich -"
+    "f /mnt/share/Share/Immich/encoded-video/.immich 0600 immich immich -"
+    "f /mnt/share/Share/Immich/library/.immich 0600 immich immich -"
+    "f /mnt/share/Share/Immich/profile/.immich 0600 immich immich -"
+    "f /mnt/share/Share/Immich/thumbs/.immich 0600 immich immich -"
+    "f /mnt/share/Share/Immich/upload/.immich 0600 immich immich -"
   ];
 
   services.immich = {
