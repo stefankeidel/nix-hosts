c23996aada5f4570533ceece7b662d98d0f0f2a6
Stefan Keidel <1188614+stefankeidel@users.noreply.github.com>
2026-06-17 10:20:49 +0200

    proxy settings

--- modules/nixos/reverse-proxy.nix
+++ modules/nixos/reverse-proxy.nix
@@ -23,12 +23,18 @@
     enableACME = true;
     extraConfig = ''
       client_max_body_size 0;
+      client_body_timeout 600s;
     '';
 
     locations."/" = {
       proxyPass = "http://127.0.0.1:2283";
       proxyWebsockets = true;
       extraConfig = ''
+        proxy_request_buffering off;
+        proxy_buffering off;
+        proxy_connect_timeout 60s;
+        proxy_send_timeout 600s;
+        proxy_read_timeout 600s;
         proxy_set_header Host $host;
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
         proxy_set_header X-Forwarded-Proto $scheme;
