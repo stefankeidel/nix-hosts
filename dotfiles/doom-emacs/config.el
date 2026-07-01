13791003dc437a5bff2d2155ff1218bf70dd00f8
Stefan Keidel <1188614+stefankeidel@users.noreply.github.com>
2026-07-01 10:12:37 +0200

    new dir

--- dotfiles/doom-emacs/config.el
+++ dotfiles/doom-emacs/config.el
@@ -40,7 +40,7 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory (file-name-as-directory (expand-file-name "~/Proton/Vault/orgmode/")))
+(setq org-directory (file-name-as-directory (expand-file-name "~/Proton/orgmode/")))
 
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
