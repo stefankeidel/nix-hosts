f3ed06fe11e55c804ef1e2d9cd7b64991a4e4e89
Stefan Keidel <1188614+stefankeidel@users.noreply.github.com>
2026-07-02 08:55:42 +0200

    nc 34

--- hosts/nixie/nextcloud.nix
+++ hosts/nixie/nextcloud.nix
@@ -8,7 +8,7 @@
   services.nextcloud = {
     enable = true;
 
-    package = pkgs.nextcloud33;
+    package = pkgs.nextcloud34;
     hostName = "cloud.keidel.me";
     https = true;
     database.createLocally = true;
