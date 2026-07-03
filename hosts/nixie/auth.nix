e6acda33cafc5ddca4ee64cac08c465b47a65ca4
Stefan Keidel <1188614+stefankeidel@users.noreply.github.com>
2026-07-03 09:49:56 +0200

    implement authelia and lldap

---
+++ hosts/nixie/auth.nix
@@ -0,0 +1,168 @@
+{ lib, ... }:
+let
+  secretFile = name: ../../secrets + "/${name}.age";
+  secretExists = name: builtins.pathExists (secretFile name);
+  requiredSecrets = [
+    "authelia-jwt-secret"
+    "authelia-storage-encryption-key"
+    "lldap-admin-password"
+    "lldap-jwt-secret"
+  ];
+  missingSecrets = builtins.filter (name: !(secretExists name)) requiredSecrets;
+  haveRequiredSecrets = missingSecrets == [ ];
+in
+{
+  assertions = [
+    {
+      assertion = haveRequiredSecrets;
+      message = ''
+        nixie auth services need these agenix secrets:
+        ${lib.concatMapStringsSep "\n" (name: "- secrets/${name}.age") missingSecrets}
+      '';
+    }
+  ];
+
+  age.secrets = lib.mkIf haveRequiredSecrets {
+    authelia-jwt-secret = {
+      file = secretFile "authelia-jwt-secret";
+      owner = "authelia-main";
+      group = "authelia-main";
+      mode = "600";
+    };
+    authelia-storage-encryption-key = {
+      file = secretFile "authelia-storage-encryption-key";
+      owner = "authelia-main";
+      group = "authelia-main";
+      mode = "600";
+    };
+    lldap-admin-password = {
+      file = secretFile "lldap-admin-password";
+      owner = "root";
+      group = "auth-secrets";
+      mode = "640";
+    };
+    lldap-jwt-secret = {
+      file = secretFile "lldap-jwt-secret";
+      owner = "lldap";
+      group = "lldap";
+      mode = "600";
+    };
+  };
+
+  users.groups.auth-secrets = { };
+  users.groups.lldap = { };
+  users.users.lldap = {
+    isSystemUser = true;
+    group = "lldap";
+    extraGroups = [ "auth-secrets" ];
+  };
+  users.users.authelia-main.extraGroups = [ "auth-secrets" ];
+
+  services.lldap = {
+    enable = true;
+    silenceForceUserPassResetWarning = true;
+
+    settings = {
+      ldap_host = "127.0.0.1";
+      ldap_port = 3890;
+      http_host = "127.0.0.1";
+      http_port = 17170;
+      http_url = "https://ldap.keidel.me";
+      ldap_base_dn = "dc=keidel,dc=me";
+      ldap_user_email = "stefan@keidel.me";
+      ldap_user_pass_file = "/run/agenix/lldap-admin-password";
+      force_ldap_user_pass_reset = "always";
+      jwt_secret_file = "/run/agenix/lldap-jwt-secret";
+    };
+  };
+
+  services.authelia.instances.main = {
+    enable = true;
+
+    secrets = {
+      jwtSecretFile = "/run/agenix/authelia-jwt-secret";
+      storageEncryptionKeyFile = "/run/agenix/authelia-storage-encryption-key";
+    };
+
+    environmentVariables = {
+      AUTHELIA_AUTHENTICATION_BACKEND_LDAP_PASSWORD_FILE = "/run/agenix/lldap-admin-password";
+    };
+
+    settings = {
+      theme = "auto";
+      default_2fa_method = "totp";
+
+      server.address = "tcp://127.0.0.1:9091/";
+
+      log = {
+        level = "info";
+        format = "text";
+      };
+
+      access_control.default_policy = "one_factor";
+
+      session = {
+        name = "authelia_session";
+        same_site = "lax";
+        cookies = [
+          {
+            domain = "keidel.me";
+            authelia_url = "https://auth.keidel.me";
+            default_redirection_url = "https://www.keidel.me";
+          }
+        ];
+      };
+
+      storage.local.path = "/var/lib/authelia-main/db.sqlite3";
+
+      authentication_backend.ldap = {
+        implementation = "lldap";
+        address = "ldap://127.0.0.1:3890";
+        base_dn = "dc=keidel,dc=me";
+        user = "uid=admin,ou=people,dc=keidel,dc=me";
+      };
+
+      notifier.filesystem.filename = "/var/lib/authelia-main/notifications.txt";
+    };
+  };
+
+  systemd.services.lldap = {
+    serviceConfig.DynamicUser = lib.mkForce false;
+  };
+
+  systemd.services.authelia-main = {
+    requires = [ "lldap.service" ];
+    after = [ "lldap.service" ];
+  };
+
+  services.nginx.virtualHosts."auth.keidel.me" = {
+    forceSSL = true;
+    enableACME = true;
+
+    locations."/" = {
+      proxyPass = "http://127.0.0.1:9091";
+      proxyWebsockets = true;
+      extraConfig = ''
+        proxy_set_header Host $host;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+        proxy_set_header X-Forwarded-Host $host;
+        proxy_set_header X-Forwarded-Uri $request_uri;
+      '';
+    };
+  };
+
+  services.nginx.virtualHosts."ldap.keidel.me" = {
+    forceSSL = true;
+    enableACME = true;
+
+    locations."/" = {
+      proxyPass = "http://127.0.0.1:17170";
+      extraConfig = ''
+        proxy_set_header Host $host;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+      '';
+    };
+  };
+}
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,14 +1,21 @@
 let
   stefan = "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAwU52M/vXuUkthu481OGKYMzFGwc9GfjvVwDLt7yQGeDXUZHx5tpL2NEKSS3imnTfOJp25wFTOAJdF63eznIOUEc+5dCZe8xeZ7IMASGlNQJy51sNUlx986BIjYxLbCl0tykkySs82ZNaog9BapjxiHm2tXb1LFR2CsGOg9mLqRVNxQkOj8KkX5+r/NhVxQRFFW8OJn7rgqsyJtA7vKRwEP+nUsokO3cr/+sWeW7APgrnnkh9iYr/ZG6ibZH/m1+t4yW1kcENVy2X8Gyrs0GWMYQCLrBB+zJYBdwxBdeWSt76QlZnOpdwWcaZEC5PUVzTiKtyUok2NjBoqdpnLezrDw==";
-  users = [stefan];
+  users = [ stefan ];
 
   lichtblick-mac = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE8aITOuJ7Z9EzI3KV1Opcy5fJldlCQ+5dzHx1QrLj5c";
   nixie = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE5v6RSePxjUpyDxV6LpU63AcI7YHSjP5jVM+DMed+/7";
   mini = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEpbyIeJsFejzHM4/r1QDYKjFg52bh/J/HZwE/wWsyCZ";
   vault = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICmSHitiberVrKGoVWpMv4xXEQGq3/ZDKExhqNeEQ+SX";
   vault-mini = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGISkJWL4EgTVKwV5CBryYvGPEWQJ7EiHn77m+wqoFFV";
-  systems = [lichtblick-mac nixie mini vault vault-mini];
-in {
+  systems = [
+    lichtblick-mac
+    nixie
+    mini
+    vault
+    vault-mini
+  ];
+in
+{
   "rclone.conf.age".publicKeys = users ++ systems;
   "restic.age".publicKeys = users ++ systems;
   "pgpass.age".publicKeys = users ++ systems;
@@ -17,4 +24,8 @@ in {
   "zsh-extra.age".publicKeys = users ++ systems;
   "navidrome.env.age".publicKeys = users ++ systems;
   "nu-extra-env.age".publicKeys = users ++ systems;
+  "authelia-jwt-secret.age".publicKeys = users ++ systems;
+  "authelia-storage-encryption-key.age".publicKeys = users ++ systems;
+  "lldap-admin-password.age".publicKeys = users ++ systems;
+  "lldap-jwt-secret.age".publicKeys = users ++ systems;
 }
