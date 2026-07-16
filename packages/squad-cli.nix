9d77b31e0359f8c529b50a9b97637b259534c197
Stefan Keidel <1188614+stefankeidel@users.noreply.github.com>
2026-07-16 11:34:29 +0200

    bump squad cli

--- packages/squad-cli.nix
+++ packages/squad-cli.nix
@@ -1,14 +1,14 @@
 { pkgs }:
 pkgs.buildNpmPackage rec {
   pname = "squad-cli";
-  version = "0.9.1";
+  version = "0.11.0";
 
   src = pkgs.fetchzip {
     url = "https://registry.npmjs.org/@bradygaster/squad-cli/-/squad-cli-${version}.tgz";
-    hash = "sha512-VL0CU55mhifaImxVUSAel0kfGDWXLfIDJL8ODbTgj3RcG4RuNeFd5iaJQbYK9D5JIdjp3jPU2D+uuCi/pnKEzg==";
+    hash = "sha256-P9CpAbF3gJUOrED3wpc07NrZhjNGYdMjeAruutxZuyw=";
   };
 
-  npmDepsHash = "sha256-4druoT9JtLcWkoWY/i1VbCJULt9feuDotZfOA8gQ1A0=";
+  npmDepsHash = "sha256-rPNLSleMTrvZbhBRs7ue6++fTKu9k8ZHvNVBFgoKsjM=";
   npmInstallFlags = [ "--omit=dev" ];
   npmPruneFlags = [ "--omit=dev" ];
   dontNpmBuild = true;
