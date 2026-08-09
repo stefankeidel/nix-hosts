9af33f6b8d61798269b831a0115b0e2e5ca554a9
Stefan Keidel <1188614+stefankeidel@users.noreply.github.com>
2026-08-09 12:41:36 +0200

    dumb it down a bit

--- modules/home/home-darwin.nix
+++ modules/home/home-darwin.nix
@@ -26,7 +26,7 @@
       qmd
       skills
     ]) ++ (with pkgs; [
-      (callPackage ../../packages/garmin-cli.nix { })
+      #(callPackage ../../packages/garmin-cli.nix { })
       (callPackage ../../packages/gccli.nix { })
       alejandra
       colima
