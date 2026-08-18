0e3062b06111140ea943040b2f1be76a932dd538
Stefan Keidel <1188614+stefankeidel@users.noreply.github.com>
2026-08-18 21:53:21 +0200

    back to ripgrep, actually

--- dotfiles/doom-emacs/config.el
+++ dotfiles/doom-emacs/config.el
@@ -269,7 +269,7 @@ that open a ghostel buffer LABEL, optionally sending COMMAND."
 ; projectile bindings
 (map! :map projectile-mode-map
       "s-p" #'projectile-command-map
-      "s-f" #'projectile-search-regexp-review);#'+default/search-project-for-symbol-at-point)
+      "s-f" #'projectile-ripgrep);#'+default/search-project-for-symbol-at-point)
 
 (map! :map projectile-command-map
       "v"   #'stefan-projectile-run-ghostel
